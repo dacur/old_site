131deb98463b39a319ea0cefab386dcecbeed226
dacur <dacur7@gmail.com>
2015-04-25 12:53:56 -0400

    Added elevator.

--- js/init.js
+++ js/init.js
@@ -1,8 +1,18 @@
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+window.onload = function() {
+  var elevator = new Elevator({
+    mainAudio: './music/audio.mp3',
+    endAudio: './music/end-audio.mp3'
+  });
+}
+
+$('.elevator').on('click', function(){
+	elevator.elevate();
+});
